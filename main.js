065b6fd0405509f62d3864a6af08fed443891b47
R-Amoedo <126144724+R-Amoedo@users.noreply.github.com>
2023-12-10 13:46:50 -0300

    Modificações
    Adicionado novo funcionário.
    
    Adicionado visualização do salario sem aumento.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function Funcionario(nome, cargo, salario) {
     this.nome = nome;
     this.cargo = cargo;
+    this.salario = salario;
     let _salario = salario;
 
     this.getSalario = function() {
@@ -29,7 +30,7 @@ function Gerente(nome) {
 }
 
 function RH(nome) {
-    Funcionario.call(this, nome, "RH", 4000);
+    Funcionario.call(this, nome, "RH", 5000);
 
     this.aumento = function() {
         const novoSalario = this.getSalario() * 1.5;
@@ -46,10 +47,20 @@ function Operacional(nome) {
     }
 }
 
+function TI(nome) {
+    Funcionario.call(this, nome, "TI", 3500);
+
+    this.aumento = function() {
+        const novoSalario = this.getSalario() * 1.5;
+        this.setSalario(novoSalario);
+    }
+}
+
 const funcionario = new Funcionario("nome", "cargo", 2000);
 const funcionario1 = new Gerente("João", "Gerente", 10000);
-const funcionario2 = new RH("Maria", "RH", 4000);
+const funcionario2 = new RH("Maria", "RH", 5000);
 const funcionario3 = new Operacional("Vitor", "Operacional", 2500);
+const funcionario4 = new TI("Carlos", "TI", 3500);
 
 funcionario1.aumento();
 console.log(funcionario1)
@@ -62,3 +73,7 @@ console.log(funcionario2.getSalario())
 funcionario3.aumento();
 console.log(funcionario3)
 console.log(funcionario3.getSalario())
+
+funcionario4.aumento();
+console.log(funcionario4)
+console.log(funcionario4.getSalario())
